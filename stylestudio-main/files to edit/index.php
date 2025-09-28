fe99b16d0e848a0321a68f4b7dca97cfabae4b87
blessingmad <mad22008@byui.edu>
2025-09-28 19:56:19 +0200

    try

--- stylestudio-main/files to edit/index.php
+++ stylestudio-main/files to edit/index.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Dencia| Home of beauty </title>
+  <title>Dencia| Home of beauty to all</title>
   <!-- Link to Font Awesome CDN -->
   <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
   <link href="styles.css" rel="stylesheet">
